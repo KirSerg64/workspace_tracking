a0b5a6d7e97eb9e1a8e6e5f5b676798ac2af4a3c
Kirill Kobychev <k.kobychev@atlsar.com>
2024-12-24 16:01:33 +0300

    data device

--- lib/train/data/loader.py
+++ lib/train/data/loader.py
@@ -84,9 +84,9 @@ def ltr_collate_stack1(batch):
             # If we're in a background process, concatenate directly into a
             # shared memory tensor to avoid an extra copy
             numel = sum([x.numel() for x in batch])
-            storage = batch[0].storage()._new_shared(numel)
+            storage = batch[0].untyped_storage()._new_shared(numel)
             out = batch[0].new(storage).view(-1, len(batch), *list(batch[0][0].shape))
-        return torch.stack(batch, 1, out=out)
+        return torch.stack(batch, 1, out=out).to("cuda")
         # if batch[0].dim() < 4:
         #     return torch.stack(batch, 0, out=out)
         # return torch.cat(batch, 0, out=out)
